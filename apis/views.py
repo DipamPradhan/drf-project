e1dd8b9350b913c7c7ae8b366c59fab48d1028f0
Dipam Pradhan <meedipam@gmail.com>
2026-02-12 17:28:32 +0545

    Implement product detail view and update product list response format

--- apis/views.py
+++ apis/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from apis.serializers import ProductSerializer
 from apis.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(["GET"])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(
@@ -10,8 +14,14 @@ def product_list(request):
         many=True,
     )
 
-    return JsonResponse(
-        {
-            "data": serializer.data,
-        }
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(
+        product,
     )
+
+    return Response(serializer.data)
